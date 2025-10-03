6627d3e696be82ea7b73cd7ed3e3cae3a7c0835a
NorteRealTrap <norterealtrp@gmail.com>
2025-10-03 10:28:14 -0300

    Fix ESLint config - remove missing extends

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,15 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react-hooks'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-    '@typescript-eslint/no-unused-vars': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 }
